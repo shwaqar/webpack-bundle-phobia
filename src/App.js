ee1863909f5bfbc36d32ade61682b4601cb28c2e
Waqar Ali <waqar@adeven.com>
2018-11-10 22:48:18 +0100

    Add bar chart component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,93 @@
 import React, { Component } from 'react';
 import 'spectre.css';
 
-import AssetsChart from './AssetsChart';
+import BarChart from './components/BarChart/BarChart';
+
+const state = [
+  {
+    name: 'Release A',
+    data: [
+      {
+        name: 'Chunk A',
+        size: 200
+      },
+      {
+        name: 'Chunk B',
+        size: 300
+      },
+      {
+        name: 'Chunk C',
+        size: 500
+      }
+    ]
+  },
+  {
+    name: 'Release B',
+    data: [
+      {
+        name: 'Chunk A',
+        size: 400
+      },
+      {
+        name: 'Chunk B',
+        size: 600
+      },
+      {
+        name: 'Chunk C',
+        size: 800
+      }
+    ]
+  },
+  {
+    name: 'Release B',
+    data: [
+      {
+        name: 'Chunk A',
+        size: 800
+      },
+      {
+        name: 'Chunk B',
+        size: 1000
+      },
+      {
+        name: 'Chunk C',
+        size: 400
+      },
+      {
+        name: 'Chunk D',
+        size: 1200
+      }
+    ]
+  },
+  {
+    name: 'Release B',
+    data: [
+      {
+        name: 'Chunk A',
+        size: 800
+      },
+      {
+        name: 'Chunk B',
+        size: 700
+      },
+      {
+        name: 'Chunk C',
+        size: 400
+      },
+      {
+        name: 'Chunk D',
+        size: 1200
+      }
+    ]
+  }
+];
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h1 className="App-title">Welcome to webpack-bundle-phobia</h1>
-        <AssetsChart />
+        <BarChart data={state} />
       </div>
     );
   }
