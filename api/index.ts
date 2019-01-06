7a860079e4ba7cc1d14758317696548e25095fa1
Waqar Ali <waqar@adeven.com>
2019-01-06 21:24:10 +0100

    Update the required config file names

--- api/index.ts
+++ api/index.ts
@@ -3,16 +3,25 @@ import findUp from 'find-up';
 import fs from 'fs';
 import { kebabCase } from 'lodash';
 
-const CONFIGFILE_NAME = '.bundlephobia.json';
+const CONFIGFILE_NAME = 'bundlephobia.json';
+const SERVICE_KEY_FILE_NAME = 'servicekey.json';
 
 const configFilePath = findUp.sync(CONFIGFILE_NAME);
+const serviceKeyFilePath = findUp.sync(SERVICE_KEY_FILE_NAME);
 
 if (!configFilePath) {
   throw new Error(`Config file with name "${CONFIGFILE_NAME}" is required!`);
 }
+if (!serviceKeyFilePath) {
+  throw new Error(
+    `Firebase service key file with name "${SERVICE_KEY_FILE_NAME}" is required!`
+  );
+}
 
 const configContent = fs.readFileSync(configFilePath, 'utf8');
+const serviceKeyContent = fs.readFileSync(serviceKeyFilePath, 'utf8');
 const config = JSON.parse(configContent);
+const serviceKey = JSON.parse(serviceKeyContent);
 
 if (!config.project_name) {
   throw new Error(`Key with 'project_name' is required!`);
@@ -20,11 +29,8 @@ if (!config.project_name) {
 if (!config.db_url) {
   throw new Error(`Key with 'db_url' is required!`);
 }
-if (!config.key) {
-  throw new Error(`Key with 'key' is required!`);
-}
 
-const firebase = firebaseInit(config.key, config.db_url);
+const firebase = firebaseInit(serviceKey, config.db_url);
 const COLLECTION = kebabCase(config.project_name);
 
 export function cleanUp() {
--- build/api/index.js
+++ build/api/index.js
@@ -7,23 +7,27 @@ const firebase_1 = __importDefault(require("./firebase"));
 const find_up_1 = __importDefault(require("find-up"));
 const fs_1 = __importDefault(require("fs"));
 const lodash_1 = require("lodash");
-const CONFIGFILE_NAME = '.bundlephobia.json';
+const CONFIGFILE_NAME = 'bundlephobia.json';
+const SERVICE_KEY_FILE_NAME = 'servicekey.json';
 const configFilePath = find_up_1.default.sync(CONFIGFILE_NAME);
+const serviceKeyFilePath = find_up_1.default.sync(SERVICE_KEY_FILE_NAME);
 if (!configFilePath) {
     throw new Error(`Config file with name "${CONFIGFILE_NAME}" is required!`);
 }
+if (!serviceKeyFilePath) {
+    throw new Error(`Firebase service key file with name "${SERVICE_KEY_FILE_NAME}" is required!`);
+}
 const configContent = fs_1.default.readFileSync(configFilePath, 'utf8');
+const serviceKeyContent = fs_1.default.readFileSync(serviceKeyFilePath, 'utf8');
 const config = JSON.parse(configContent);
+const serviceKey = JSON.parse(serviceKeyContent);
 if (!config.project_name) {
     throw new Error(`Key with 'project_name' is required!`);
 }
 if (!config.db_url) {
     throw new Error(`Key with 'db_url' is required!`);
 }
-if (!config.key) {
-    throw new Error(`Key with 'key' is required!`);
-}
-const firebase = firebase_1.default(config.key, config.db_url);
+const firebase = firebase_1.default(serviceKey, config.db_url);
 const COLLECTION = lodash_1.kebabCase(config.project_name);
 function cleanUp() {
     return firebase.delete();
